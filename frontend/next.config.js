244d56d2d47b9f4f566b64883fa52ea6977a6a23
Aadi Manchekar <aadiprakash.manchekar2019@vitstudent.ac.in>
2022-06-28 17:24:31 +0530

    fixed prop className did not match error

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  compiler: {
+    styledComponents: true,
+  },
+  experimental: {
+    // Enables the styled-components SWC transform
+    styledComponents: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
